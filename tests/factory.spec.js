02bb6877b1ef5f1c93fcc8278bf75075809227cf
Esteban Echeverry <eecheverry@nubark.com>
2019-02-12 22:46:56 -0500

    Remove unused code and setup coverage

---
+++ tests/factory.spec.js
@@ -0,0 +1,18 @@
+import { Factory } from '../lib/factory.js'
+
+
+describe('Factory interface', function () {
+    beforeEach(function () {
+      this.factory = new Factory()
+    })
+  
+    it('has an abstract extract method', function () {
+        try {
+            this.factory.extract('factoryMethod')
+        } catch(error) {
+            expect(error).toBeTruthy()
+        }
+      
+    })
+  
+})
