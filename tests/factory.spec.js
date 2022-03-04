0b1f5979a98e5e9e1ddfb045b6dfe94935b975f3
Esteban Echeverry <eecheverry@knowark.com>
2022-03-04 17:52:30 +0000

    Receive config parameter in factory.

--- tests/factory.spec.js
+++ tests/factory.spec.js
@@ -4,13 +4,19 @@ describe('Factory interface', function () {
   let factory = null
   beforeEach(function () {
     factory = new Factory()
-    factory.factoryMethod = function () {
+    factory.factoryMethod = () => {
       return 'ResolvedResource'
     }
   })
 
-  it('has a default extract method', function () {
+  it('has a default extract method', () => {
     const method = factory.extract('factoryMethod')
     expect(method()).toEqual('ResolvedResource')
+  })
+
+  it('can be instantiated with a config object', () => {
+    const config = { key: 'value' }
+    factory = new Factory(config)
+    expect(factory.config).toBe(config)
   })
 })
