26f6ce65671483993ea88db59403bf3195f56931
Esteban Echeverry <eecheverry@knowark.com>
2022-11-11 13:49:00 -0500

    Add type definitions.

---
+++ lib/index.d.ts
@@ -0,0 +1,27 @@
+export declare class Factory {
+  constructor(config: object)
+
+  config: object
+
+  allowed: string[]
+
+  extract(method: string): Function
+}
+
+export declare class Injectark {
+  constructor(dependencies: {
+    factory: Factory,
+    strategy?: object,
+    parent?: Injectark
+  })
+
+  config: object
+
+  get(resource: string): unknown
+
+  set(resource: string, instance: unknown): void
+
+  resolve(resource: string, strict: boolean): unknown
+
+  forge(dependencies: { strategy?: object, factory?: Factory }): Injectark
+}
